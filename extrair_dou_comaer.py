2cc59ae7b4946e4c6aa38c563d2bd0a56ee1abd1
Cap Labrego <labregofgl@comgap.intraer>
2025-03-18 10:32:10 -0300

    adding headers

--- extrair_dou_comaer.py
+++ extrair_dou_comaer.py
@@ -92,8 +92,20 @@ def get_pub_content(url):
 
     url = f"https://www.in.gov.br/web/dou/-/{url}"
 
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Connection": "keep-alive"
+    }
+
+   
     # Fazer a solicitação HTTP com o proxy configurado através das variáveis de ambiente
-    response = requests.post(url, verify=False)
+    response = requests.post(url, headers=headers, verify=False)
+
+    print(response.status_code)
+    print(response.text[:500])  # Exibe os primeiros 500 caracteres da resposta
 
     # Verificar se há resultados ou não
     soup = BeautifulSoup(response.text, 'html.parser')
